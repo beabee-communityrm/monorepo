30935542b5343bcd6fcbd290b161d384d72be45d
Will Franklin <will@willpf.co.uk>
2023-10-13 16:53:45 +0100

    Add nextSlideId

--- src/data/callouts.ts
+++ src/data/callouts.ts
@@ -41,7 +41,7 @@ export interface RadioCalloutComponentSchema
   extends BaseCalloutComponentSchema {
   type: "radio" | "selectboxes";
   input: true;
-  values: { label: string; value: string }[];
+  values: { label: string; value: string; nextSlideId: string }[];
 }
 
 export type CalloutComponentSchema =
