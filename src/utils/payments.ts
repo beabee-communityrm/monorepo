af54c81596963049d2dc78bec175a57101ac33d0
Pascal Garber <pascal@artandcode.studio>
2024-03-21 15:11:30 +0100

    feat(payment-paypal): Add paypal fees

--- src/utils/payments.ts
+++ src/utils/payments.ts
@@ -8,19 +8,19 @@ const stripeFees = {
     [PaymentMethod.StripeSEPA]: () => 0.3,
     [PaymentMethod.StripeBACS]: (amount: number) =>
       Math.min(2, Math.max(0.2, 0.01 * amount)),
-    [PaymentMethod.StripePayPal]: () => 0, // TODO: Check
+    [PaymentMethod.StripePayPal]: (amount: number) => 0.1 + 0.020 * amount,
   },
   eu: {
     [PaymentMethod.StripeCard]: (amount: number) => 0.25 + 0.015 * amount,
     [PaymentMethod.StripeSEPA]: () => 0.35,
     [PaymentMethod.StripeBACS]: () => 0, // Not available
-    [PaymentMethod.StripePayPal]: () => 0, // TODO: Check
+    [PaymentMethod.StripePayPal]: (amount: number) => 0.1 + 0.020 * amount,
   },
   ca: {
     [PaymentMethod.StripeCard]: (amount: number) => 0.3 + 0.029 * amount,
     [PaymentMethod.StripeSEPA]: () => 0, // Not available
     [PaymentMethod.StripeBACS]: () => 0, // Not available
-    [PaymentMethod.StripePayPal]: () => 0, // TODO: Check
+    [PaymentMethod.StripePayPal]: () => 0, // Not available
   },
 } as const;
 
