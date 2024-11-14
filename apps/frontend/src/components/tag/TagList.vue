43e3e0f053260d38f4f1376100976882284d51a1
Pascal Garber <pascal@artandcode.studio>
2024-11-14 19:29:54 +0100

    fix(contact-tagging): Frontend: Wrap tags if there is not enough space

--- apps/frontend/src/components/tag/TagList.vue
+++ apps/frontend/src/components/tag/TagList.vue
@@ -1,13 +1,15 @@
 <template>
-  <div v-if="tags.length > 0" class="flex items-center gap-2">
-    <font-awesome-icon :icon="faTag" class="mr-2" />
-    <AppTag
-      v-for="tag in tags"
-      :key="tag.id"
-      :tag="tag.name"
-      class="cursor-pointer hover:opacity-80"
-      @click="handleTagClick(tag.id)"
-    />
+  <div v-if="tags.length > 0" class="ml-4">
+    <font-awesome-icon :icon="faTag" class="absolute -ml-4 mt-1" />
+    <div class="flex flex-wrap items-center gap-2">
+      <AppTag
+        v-for="tag in tags"
+        :key="tag.id"
+        :tag="tag.name"
+        class="cursor-pointer hover:opacity-80"
+        @click="handleTagClick(tag.id)"
+      />
+    </div>
   </div>
 </template>
 
