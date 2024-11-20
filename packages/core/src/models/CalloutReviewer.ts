368d357165430be07d47608816ed6e8bd60e1bc1
Will Franklin <will@willpf.co.uk>
2024-11-20 11:23:16 +0100

    Add callout review model

---
+++ packages/core/src/models/CalloutReviewer.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Unique
+} from "typeorm";
+import type { Callout } from "./Callout";
+import type { Contact } from "./Contact";
+
+@Entity()
+@Unique(["callout", "contact"])
+export default class CalloutReviewer {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  calloutId!: string;
+  @ManyToOne("Callout")
+  callout!: Callout;
+
+  @Column()
+  contactId!: string;
+  @ManyToOne("Contact")
+  contact!: Contact;
+
+  @CreateDateColumn()
+  date!: Date;
+}
