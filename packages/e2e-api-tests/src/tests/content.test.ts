7e88a012bd53a62cbe9948e437ca1234a68dad28
Pascal Garber <pascal@artandcode.studio>
2024-11-22 19:30:58 +0100

    feat(e2e-api-test): Experimental github action

--- packages/e2e-api-tests/src/tests/content.test.ts
+++ packages/e2e-api-tests/src/tests/content.test.ts
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 
 const HOST = process.env.APP_BASE_URL || 'http://localhost:3002';
 const PATH = process.env.APP_BASE_URL || '/api/1.0';
+const IS_GITHUB_ACTIONS = process.env.GITHUB_ACTIONS === 'true';
 
 dotenv.config({ path: ['.env', '.env.example'] });
 
