d2ae2a4a7d12be221613626c9196ffa95925f53f
Will Franklin <will@willpf.co.uk>
2024-12-20 13:13:00 +0000

    Upgrade ESLint

---
+++ apps/frontend/eslint.config.mjs
@@ -0,0 +1,34 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import typescriptEslint from 'typescript-eslint';
+
+export default typescriptEslint.config(
+  { ignores: ['*.d.ts', '**/coverage', '**/dist', '**/vite.config.*'] },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+      ...eslintPluginVue.configs['flat/recommended'],
+    ],
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        parser: typescriptEslint.parser,
+      },
+    },
+    rules: {
+      'vue/no-v-html': 'off',
+      'no-console': 'warn',
+    },
+  },
+  {
+    files: ['src/pages/**/*.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    },
+  },
+  eslintConfigPrettier
+);
