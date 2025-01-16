77a8f618a4bc4562a30aa972f09e19663a83831b
Pascal Garber <pascal@artandcode.studio>
2025-01-16 13:04:10 +0100

    test(cleanup): Readd accidentally deleted vitest.config.ts file

---
+++ packages/client/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['test/**/*.test.ts'],
+    
+    globalSetup: [
+      '@beabee/test-utils/vitest/docker-compose-setup'
+    ],
+    
+    environment: 'node'
+  }
+}) 
