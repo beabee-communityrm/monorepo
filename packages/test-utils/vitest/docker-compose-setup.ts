3f40ecaa7d4b58801570e1b2c5605b068c7998f1
Pascal Garber <pascal@artandcode.studio>
2025-01-16 12:08:01 +0100

    test(viteset): Migrate client and e2e jest tests to vitest

---
+++ packages/test-utils/vitest/docker-compose-setup.ts
@@ -0,0 +1,47 @@
+import { DockerComposeEnvironment, type StartedDockerComposeEnvironment, Wait } from "testcontainers";
+import path from "node:path";
+
+const composeFilePath = path.resolve(process.cwd() + "/../..");
+const testUserEmail = "test@beabee.io";
+const createTestUserCommand = `yarn backend-cli user create --firstname Test --lastname Test --email ${testUserEmail}`;
+const createTestApiKeyCommand = `yarn backend-cli api-key create --description api-tests --email ${testUserEmail}`;
+
+let startedDockerComposeEnvironment: StartedDockerComposeEnvironment | null = null;
+
+export async function setup() {
+  console.log("Starting Docker Compose environment...");
+
+  startedDockerComposeEnvironment = await new DockerComposeEnvironment(
+    composeFilePath,
+    "docker-compose.test.yml"
+  )
+    .withWaitStrategy(
+      "db",
+      Wait.forLogMessage(/database system is ready to accept connections/)
+    )
+    .withWaitStrategy(
+      "api_app",
+      Wait.forLogMessage(/Server is ready and listening on port 3000/)
+    )
+    .up(["db", "migration", "api_app", "app_router"]);
+
+  const apiApp = startedDockerComposeEnvironment.getContainer("api_app-1");
+
+  await apiApp.exec(createTestUserCommand.split(" "));
+
+  const apiKeyOutput = await apiApp.exec(createTestApiKeyCommand.split(" "));
+
+  const token = apiKeyOutput.output.match(/Token: (.+)/)?.[1];
+  if (token) {
+    console.log("Test API key created:", token);
+    process.env.API_KEY = token.trim();
+  }
+
+} 
+
+export async function teardown() {
+    console.log("Tearing down Docker Compose environment...");
+    if (startedDockerComposeEnvironment) {
+      await startedDockerComposeEnvironment.down();
+    }
+  }
