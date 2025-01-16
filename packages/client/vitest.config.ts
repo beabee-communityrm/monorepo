d909d92fc94f56276a5d5afbfedc23deea936c4f
Pascal Garber <pascal@artandcode.studio>
2025-01-16 13:57:22 +0100

    test(viteset): Remove the last remaining jest files

--- packages/client/vitest.config.ts
+++ packages/client/vitest.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ['test/**/*.test.ts'],
-    
-    globalSetup: [
-      '@beabee/test-utils/vitest/docker-compose-setup'
-    ],
-    
-    environment: 'node'
+    include: ["test/**/*.test.ts"],
+
+    globalSetup: ["@beabee/test-utils/vitest/docker-compose-setup"],
+
+    environment: "node"
   }
-}) 
+});
