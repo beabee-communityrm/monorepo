8730058e7332f088315e485d3bb9009e1afa4153
Will Franklin <will@willpf.co.uk>
2025-01-28 13:40:02 +0100

    Remove inherited properties from NewsletterContact

--- packages/core/src/type/newsletter-contact.ts
+++ packages/core/src/type/newsletter-contact.ts
@@ -1,12 +1,6 @@
-import { NewsletterStatus } from "@beabee/beabee-common";
 import { UpdateNewsletterContact } from "./update-newsletter-contact";
 
 export interface NewsletterContact extends UpdateNewsletterContact {
-  firstname: string;
-  lastname: string;
   joined: Date;
-  status: NewsletterStatus;
-  groups: string[];
   tags: string[];
-  fields: Record<string, string>;
 }
