021dca39e02a6a78126f9045ab26c94bcb66a59d
Pascal Garber <pascal@artandcode.studio>
2025-01-31 11:01:07 +0100

    feat(backend-cli): Option added to execute the cli locally

--- apps/backend-cli/src/env.ts
+++ apps/backend-cli/src/env.ts
@@ -1,21 +1,27 @@
 import dotenv from "dotenv";
-import { resolve } from "path";
+import { resolve } from "node:path";
+import { existsSync } from "node:fs";
 
-// Define default values
-const defaults = {
-  NODE_ENV: "development"
-  // Add more default values as needed
-};
+const isRunningInDocker = () => existsSync("/.dockerenv");
 
-// Load environment variables with fallback to defaults
-const env = {
-  ...defaults,
-  ...dotenv.config({ path: resolve("./.env") }).parsed
-};
+// This CLI is designed to be used in the docker environment but can also be used locally for testing.
+// For this we need to load the .env, core package .env and .env.test files.
+// We do not need to load the .env.test files if we are inside the docker environment
+// because the env variables are set in the docker compose file.
+const workspaceRootPath = resolve(process.cwd() + "/../..");
 
-// Set environment variables
-Object.entries(env).forEach(([key, value]) => {
-  process.env[key] ??= value;
-});
+let env = { ...process.env };
 
+if (!isRunningInDocker()) {
+  env = { ...env, ...dotenv.config({ path: [workspaceRootPath + "/.env.cli"], override: false }).parsed };
+  env = { ...env, ...dotenv.config({ path: [workspaceRootPath + "/packages/core/.env"], override: false }).parsed };
+  env = { ...env, ...dotenv.config({ path: [workspaceRootPath + "/.env"], override: false }).parsed };
+
+  if (process.env.NODE_ENV === "test") {
+    env = { ...env, ...dotenv.config({ path: [workspaceRootPath + "/.env.test"], override: true }).parsed };
+    env = { ...env, ...dotenv.config({ path: [workspaceRootPath + "/.env.cli-test"], override: true }).parsed };
+  }
+}
+
+export { env };
 export default env;
