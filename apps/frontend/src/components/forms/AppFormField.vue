604edec8e54c57b4d1bf736da0e360db9a81a6d8
Pascal Garber <pascal@artandcode.studio>
2025-02-19 14:02:24 +0100

    feat: add AppFormBox and AppFormField components
    Introduce AppFormBox and AppFormField components to enhance form 
    structure and usability. AppFormBox provides a labeled container for 
    form elements, while AppFormField allows for help text to assist 
    users. These components improve the overall user experience and 
    maintain consistency in form design.

---
+++ apps/frontend/src/components/forms/AppFormField.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mb-4 last:mb-0">
+    <slot />
+    <div
+      v-if="help"
+      class="content-i18n mt-2 text-sm text-grey"
+      v-html="help"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  help?: string;
+}>();
+</script>
