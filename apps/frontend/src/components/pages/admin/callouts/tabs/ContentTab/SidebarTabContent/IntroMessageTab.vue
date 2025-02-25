2a9cd2b09301a6b8d7742ad2a6c6cd95452a8364
Pascal Garber <pascal@artandcode.studio>
2025-02-25 15:27:48 +0100

    feat(callout-admin-ux): Introduction text layout

--- apps/frontend/src/components/pages/admin/callouts/tabs/ContentTab/SidebarTabContent/IntroMessageTab.vue
+++ apps/frontend/src/components/pages/admin/callouts/tabs/ContentTab/SidebarTabContent/IntroMessageTab.vue
@@ -1,22 +1,31 @@
 <!-- eslint-disable vue/no-mutating-props -->
 <template>
-  <div class="flex max-h-full min-h-0 flex-1 flex-col">
-    <AppFormSection>
-      <!-- TODO: Change locale key path to calloutBuilder.intro.label -->
-      <LocaleRichTextEditor
-        v-model="data.introText"
-        :locales="locales"
-        :label="t('createCallout.tabs.intro.label')"
-        required
-      />
-    </AppFormSection>
+  <div class="flex max-h-full min-h-0 flex-1">
+    <AppFormBox class="flex-1 overflow-y-auto bg-white p-6 shadow-md">
+      <!-- TODO: Move help text to the top; See draft -->
+      <AppFormField
+        class="mx-auto max-w-3xl"
+        :help="t('createCallout.tabs.intro.help')"
+      >
+        <!-- TODO: Change locale key path to calloutBuilder.intro.label -->
+        <LocaleRichTextEditor
+          v-model="data.introText"
+          :locales="locales"
+          :label="t('createCallout.tabs.intro.label')"
+          required
+        />
+      </AppFormField>
+    </AppFormBox>
+    <!-- Right Sidebar -->
+    <div class="flex-0 basis-[15rem] overflow-y-auto" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n';
-import AppFormSection from '@components/forms/AppFormSection.vue';
+import AppFormField from '@components/forms/AppFormField.vue';
 import LocaleRichTextEditor from '@components/forms/LocaleRichTextEditor.vue';
+
 import type { LocaleProp } from '@type';
 import type { SidebarTabProps } from '../SidebarTabs.interface';
 
