36e55d68d0fa2ab811ddf64bceef351ce94a4019
Will Franklin <will@willpf.co.uk>
2025-03-26 16:12:59 +0100

    Add showNewsletterOptIn column, remove long unused responsePassword

---
+++ packages/core/src/migrations/1743001845903-AddNLOptInRemovePwCallouts.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddNLOptInRemovePwCallouts1743001845903
+  implements MigrationInterface
+{
+  name = "AddNLOptInRemovePwCallouts1743001845903";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "callout" DROP COLUMN "responsePassword"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "callout" ADD "showNewsletterOptIn" boolean NOT NULL DEFAULT false`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "callout" DROP COLUMN "showNewsletterOptIn"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "callout" ADD "responsePassword" character varying`
+    );
+  }
+}
