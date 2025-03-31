21e735a18031090d307e829863a1c23c2b3233eb
Will Franklin <will@willpf.co.uk>
2025-03-31 11:25:49 +0200

    Tags should only load for admins

--- apps/backend/src/api/dto/ContactTagDto.ts
+++ apps/backend/src/api/dto/ContactTagDto.ts
@@ -1,6 +1,24 @@
+import { IsString, IsUUID } from "class-validator";
 import { CreateTagDto, GetTagDto, ListTagsDto } from "./TagDto";
 
-export class GetContactTagDto extends GetTagDto {}
+/**
+ * The contact tag DTO
+ *
+ * These tags are only visible to admins. This is enforced here due to
+ * limitations in class-validator
+ *
+ * 1) It should be in GetContactDto, but group validation with ValidateNested
+ *    doesn't work
+ * 2) We should at least be able to extend GetTagDto instead of implementing it,
+ *    but adding groups via inheritance doesn't work
+ */
+export class GetContactTagDto implements GetTagDto {
+  @IsUUID(undefined, { groups: ["admin"] })
+  id!: string;
+
+  @IsString()
+  name!: string;
+}
 
 export class CreateContactTagDto extends CreateTagDto {}
 
