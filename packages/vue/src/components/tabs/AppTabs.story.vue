947359a0c06b88ee5675e6aceaa046871523afd1
Will Franklin <will@willpf.co.uk>
2025-04-03 15:38:41 +0200

    Fix broken imports

--- packages/vue/src/components/tabs/AppTabs.story.vue
+++ packages/vue/src/components/tabs/AppTabs.story.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { reactive } from 'vue';
-import { AppTabs, type AppTabsProps, type TabItem } from './index';
+import { AppTabs, type AppTabsProps } from './index';
+import type { TabItem } from '../../types';
 
 const state = reactive({
   selectedTab: 'tab1',
