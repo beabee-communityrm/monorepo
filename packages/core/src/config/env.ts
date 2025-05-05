e93e5715a7252c42f5680ce0cf45d51796ac4cb8
Will Franklin <will@willpf.co.uk>
2025-05-05 17:14:41 +0200

    Add ability to encode config vars as base64

--- packages/core/src/config/env.ts
+++ packages/core/src/config/env.ts
@@ -1,13 +1,29 @@
 import { strict as assert } from "assert";
 
 /**
+ * Reads an environment variable and decodes it if it is base64 encoded.
+ * @param name The name of the environment variable to read
+ * @returns The value of the environment variable, or undefined if it is not set
+ */
+function read(name: string): string | undefined {
+  const value = process.env[name];
+  if (value?.startsWith("base64:")) {
+    const base64Value = value.substring(7);
+    const decodedValue = Buffer.from(base64Value, "base64").toString("utf-8");
+    return decodedValue;
+  } else {
+    return value;
+  }
+}
+
+/**
  * Reads an environment variable as a string
  * @param name The name of the environment variable to read
  * @param def The default value to return if the environment variable is not set
  * @returns The value
  */
 export function s(name: string, def?: string): string {
-  const value = process.env[name] || def;
+  const value = read(name) || def;
   assert(value !== undefined, `Missing environment variable ${name}`);
   return value;
 }
@@ -19,7 +35,7 @@ export function s(name: string, def?: string): string {
  * @returns The value
  */
 export function ss(name: string, def?: string[]): string[] {
-  const value = process.env[name]?.split(",") || def;
+  const value = read(name)?.split(",") || def;
   assert(value !== undefined, `Missing environment variable ${name}`);
   return value;
 }
@@ -31,11 +47,11 @@ export function ss(name: string, def?: string[]): string[] {
  * @returns The value
  */
 export function n(name: string, def?: number): number {
-  const value = Number(process.env[name]) || def;
+  const value = Number(read(name)) || def;
   assert(value !== undefined, `Missing environment variable ${name}`);
   assert(
     !isNaN(value),
-    `Invalid number for environment variable ${name}: ${process.env[name]}`
+    `Invalid number for environment variable ${name}: ${value}`
   );
   return value;
 }
@@ -47,7 +63,7 @@ export function n(name: string, def?: number): number {
  * @returns The value
  */
 export function b(name: string, def?: boolean): boolean {
-  const value = process.env[name];
+  const value = read(name);
   if (value === undefined && def !== undefined) {
     return def;
   }
@@ -87,7 +103,7 @@ export function e<T extends readonly string[]>(
  * @returns The value
  */
 export function json(name: string, def?: any): any {
-  const value = process.env[name];
+  const value = read(name);
   assert(
     value !== undefined || def !== undefined,
     `Missing environment variable ${name}`
