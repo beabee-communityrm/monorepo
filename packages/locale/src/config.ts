28e7629c3959ce4ac14fea9172c073f9ad82c04e
Pascal Garber <pascal@artandcode.studio>
2025-05-30 17:34:14 +0200

    refactor(locale): Replace JSON config with TypeScript object
    - Removed the `config.json` file and integrated its content directly into `config.ts` as a TypeScript object for better type safety and maintainability.
    - Updated the `Locale` type definition to explicitly list available locales, enhancing clarity and reducing reliance on JSON imports.

--- packages/locale/src/config.ts
+++ packages/locale/src/config.ts
@@ -1,4 +1,70 @@
-import _config from "./config.json";
 import type { LocaleOptions } from "./types/locale-option.js";
+import { LocaleContext } from "./locale-context.js";
 
-export const config: LocaleOptions = _config as LocaleOptions;
+export const config: LocaleOptions = {
+  en: {
+    baseLocale: "en",
+    name: "English",
+    displayName: "English",
+    adminLocale: "en",
+    availableIn: [LocaleContext.Callout, LocaleContext.System],
+  },
+  de: {
+    baseLocale: "de",
+    name: "Deutsch (formal)",
+    displayName: "Deutsch",
+    adminLocale: "de",
+    availableIn: [LocaleContext.Callout, LocaleContext.System],
+  },
+  "de@informal": {
+    baseLocale: "de",
+    name: "Deutsch (informal)",
+    displayName: "Deutsch",
+    adminLocale: "de@informal",
+    fallbackLocale: "de",
+    availableIn: [LocaleContext.Callout, LocaleContext.System],
+  },
+  "de@easy": {
+    baseLocale: "de",
+    name: "Deutsch (einfach)",
+    displayName: "Leichte Sprache",
+    adminLocale: "de",
+    fallbackLocale: "de@informal",
+    availableIn: [LocaleContext.Callout],
+  },
+  nl: {
+    baseLocale: "nl",
+    name: "Nederlands",
+    displayName: "Nederlands",
+    adminLocale: "nl",
+    availableIn: [LocaleContext.Callout, LocaleContext.System],
+  },
+  pt: {
+    baseLocale: "pt",
+    name: "Português",
+    displayName: "Português",
+    adminLocale: "en",
+    availableIn: [LocaleContext.Callout, LocaleContext.System],
+  },
+  it: {
+    baseLocale: "it",
+    name: "Italiano",
+    displayName: "Italiano",
+    adminLocale: "en",
+    availableIn: [LocaleContext.Callout, LocaleContext.System],
+  },
+  ru: {
+    baseLocale: "ru",
+    name: "Русский",
+    displayName: "Русский",
+    adminLocale: "en",
+    availableIn: [LocaleContext.Callout, LocaleContext.System],
+  },
+  fr: {
+    baseLocale: "fr",
+    name: "Français",
+    displayName: "Français",
+    adminLocale: "fr",
+    availableIn: [LocaleContext.Callout, LocaleContext.System],
+  },
+};
