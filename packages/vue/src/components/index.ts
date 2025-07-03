cad8b8f1cfe030eebc91d5e818cf62a08b172155
Pascal Garber <pascal@artandcode.studio>
2025-07-03 14:25:52 +0200

    WIP: Search components migration - Vue package working, Frontend needs 63 fixes
    - Successfully moved 13 search components to frontend
    - Vue package compiles cleanly
    - Frontend has complex import/type dependencies that need resolution
    - Search system is more complex than other migrated components

--- packages/vue/src/components/index.ts
+++ packages/vue/src/components/index.ts
@@ -10,7 +10,6 @@ export * from './list';
 export * from './navigation';
 export * from './notification';
 export * from './scroll';
-export * from './search';
 export * from './status';
 export * from './table';
 export * from './tabs';
