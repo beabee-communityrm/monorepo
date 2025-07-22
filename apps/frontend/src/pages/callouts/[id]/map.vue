3be922068ee6243cdc665050d28c095391c66603
RayMille <camille.koenders@gmail.com>
2025-07-22 17:29:10 +0200

    refactor(frontend): move image functionality to utilities

--- apps/frontend/src/pages/callouts/[id]/map.vue
+++ apps/frontend/src/pages/callouts/[id]/map.vue
@@ -203,12 +203,7 @@ import {
   HASH_PREFIX,
   useCallout,
 } from '@components/pages/callouts/use-callout';
-import { icon } from '@fortawesome/fontawesome-svg-core';
-import {
-  type IconName,
-  faInfoCircle,
-  faPlus,
-} from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { currentLocaleConfig } from '@lib/i18n';
 import { GeocodingControl } from '@maptiler/geocoding-control/maplibregl';
 import '@maptiler/geocoding-control/style.css';
@@ -220,7 +215,7 @@ import type {
   MapPointFeature,
   MapPointFeatureCollection,
 } from '@type';
-import { setKey } from '@utils';
+import { generateImageId, getImageString, setKey, svgToImage } from '@utils';
 import { client } from '@utils/api';
 import {
   type GeocodeResult,
@@ -750,80 +745,6 @@ function handleLoad({ map: mapInstance }: { map: Map }) {
         });
     }
   }
-}
-
-/**
- * Generate a unique ID for an icon image based on its name and color
- *
- * @param iconName The name of the icon
- * @param color The color of the icon
- * @returns A unique ID for the icon
- */
-function generateImageId(iconName: string, color: string): string {
-  return `icon-${iconName}-${color}`;
-}
-
-/**
- * Convert an SVG string to a PNG data URL
- *
- * @param svgString The SVG string to convert
- * @returns A promise that resolves to the PNG data URL
- */
-function svgToImage(svgString: string): Promise<string> {
-  return new Promise<string>((resolve, reject) => {
-    const img = new Image();
-    const svgBlob = new Blob([svgString], {
-      type: 'image/svg+xml;charset=utf-8',
-    });
-    const url = URL.createObjectURL(svgBlob);
-
-    img.onload = () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = img.width;
-      canvas.height = img.height;
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        ctx.drawImage(img, 0, 0);
-        const pngDataUrl = canvas.toDataURL('image/png');
-        resolve(pngDataUrl);
-      } else {
-        reject(new Error('Failed to get canvas context'));
-      }
-      URL.revokeObjectURL(url);
-    };
-
-    img.onerror = (error) => reject(error);
-    img.src = url;
-  });
-}
-
-/**
- * Get the SVG string for a FontAwesome icon with a specific color
- *
- * @param iconName The name of the icon
- * @param color The color to use for the icon
- * @returns The SVG string for the icon
- */
-function getImageString(iconName: string, color: string): string {
-  const iconObject = icon({ prefix: 'fas', iconName: iconName as IconName });
-
-  const svgPath = iconObject.icon[4] as string;
-  const width = iconObject.icon[0] as number;
-  const height = iconObject.icon[1] as number;
-
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  const scale = 0.05;
-  svg.setAttribute('width', (width * scale).toString());
-  svg.setAttribute('height', (height * scale).toString());
-  svg.setAttribute('viewBox', `0 0 ${width} ${height}`);
-
-  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  path.setAttribute('d', svgPath);
-  path.setAttribute('fill', color);
-
-  svg.appendChild(path);
-
-  return new XMLSerializer().serializeToString(svg);
 }
 
 /**
