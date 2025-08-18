13c4d9a4e7b078a9612222cf11751e9498184b48
RayMille <camille.koenders@correctiv.org>
2025-08-18 18:32:01 +0200

    feat(backend): add callout response endpoints

---
+++ packages/common/src/types/get-callout-response-segment-with.ts
@@ -0,0 +1,1 @@
+export type GetCalloutResponseSegmentWith = 'calloutResponseCount' | void;
---
+++ packages/common/src/types/update-callout-response-segment-data.ts
@@ -0,0 +1,3 @@
+import { CreateCalloutResponseSegmentData } from './index.js';
+
+export type UpdateCalloutResponseSegmentData = Partial<CreateCalloutResponseSegmentData>;
