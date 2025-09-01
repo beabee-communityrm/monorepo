30185781cba8cf9f95f5aad4a0ca02134dc63ada
RayMille <camille.koenders@correctiv.org>
2025-09-01 12:50:05 +0200

    map inbox bucket correctly to callout responses (inbox => bucket = '')

--- packages/core/src/migrations/1755526570700-AddDefaultCalloutResponseSegments.ts
+++ packages/core/src/migrations/1755526570700-AddDefaultCalloutResponseSegments.ts
@@ -5,7 +5,7 @@ export class AddDefaultCalloutResponseSegments1755526570700
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO public.callout_response_segment (id, name, "ruleGroup", "order") VALUES ('81664449-adf2-4c3a-aee2-145a96d67726', 'Inbox', '{"rules": [{"field": "bucket", "value": ["inbox"], "operator": "equal"}], "condition": "AND"}', 0)`
+      `INSERT INTO public.callout_response_segment (id, name, "ruleGroup", "order") VALUES ('81664449-adf2-4c3a-aee2-145a96d67726', 'Inbox', '{"rules": [{"field": "bucket", "value": [""], "operator": "equal"}], "condition": "AND"}', 0)`
     );
     await queryRunner.query(
       `INSERT INTO public.callout_response_segment (id, name,  "ruleGroup", "order") VALUES ('ce1b2919-85c1-4134-8231-df6b860c0ae2', 'Verified',  '{"rules": [{"field": "bucket", "value": ["verified"], "operator": "equal"}], "condition": "AND"}', 1)`
