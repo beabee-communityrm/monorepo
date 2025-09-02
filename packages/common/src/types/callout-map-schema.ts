ee3c914f9bc93d48ebe91a083b85f7ac393eecf9
Pascal Garber <pascal@artandcode.studio>
2025-09-02 10:01:04 +0200

    docs(common): add geocodeCountries property documentation

--- packages/common/src/types/callout-map-schema.ts
+++ packages/common/src/types/callout-map-schema.ts
@@ -10,6 +10,11 @@ export interface CalloutMapSchema {
   addressProp: string;
   addressPattern: string;
   addressPatternProp: string;
+  /**
+   * Restricts geocoding search results to specific countries. This configuration parameter
+   * can be set through the admin interface and is passed to the MapTiler GeocodingControl
+   * to limit search results to the specified countries.
+   */
   geocodeCountries?: string;
   mapIconProp?: string;
   mapIconStyling?: CalloutMapSchemaIconStyling;
