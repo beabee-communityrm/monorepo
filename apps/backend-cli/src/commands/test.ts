e20d7f36332b65137944d322e8e259a3a375b3ff
RayMille <camille.koenders@correctiv.org>
2025-09-24 14:25:01 +0200

    feat(backend-cli): add anonymiser to backend-cli

--- apps/backend-cli/src/commands/test.ts
+++ apps/backend-cli/src/commands/test.ts
@@ -4,19 +4,38 @@ export const testCommand: CommandModule = {
   command: 'test <action>',
   describe: 'Test environment commands',
   builder: (yargs) =>
-    yargs.command({
-      command: 'list-users',
-      describe: 'List test users with various contribution scenarios',
-      builder: (yargs) =>
-        yargs.option('dryRun', {
-          type: 'boolean',
-          description: 'Run without making changes',
-          default: false,
-        }),
-      handler: async (argv) => {
-        const { listTestUsers } = await import('../actions/test/list-users.js');
-        return listTestUsers(argv.dryRun);
-      },
-    }),
+    yargs
+      .command({
+        command: 'list-users',
+        describe: 'List test users with various contribution scenarios',
+        builder: (yargs) =>
+          yargs.option('dryRun', {
+            type: 'boolean',
+            description: 'Run without making changes',
+            default: false,
+          }),
+        handler: async (argv) => {
+          const { listTestUsers } = await import(
+            '../actions/test/list-users.js'
+          );
+          return listTestUsers(argv.dryRun);
+        },
+      })
+      .command({
+        command: 'anonymise',
+        describe: 'Create anonymized copy of database and export data to SQL dump',
+        builder: (yargs) =>
+          yargs.option('dryRun', {
+            type: 'boolean',
+            description: 'Run without making changes',
+            default: false,
+          }),
+        handler: async () => {
+          const { runAnonymisers } = await import(
+            '../actions/test/anonymise.js'
+          );
+          return runAnonymisers();
+        },
+      }),
   handler: () => {},
 };
