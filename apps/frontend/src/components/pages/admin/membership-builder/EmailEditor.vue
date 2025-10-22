401c9aa52f17589ea37ec6814524779eb1ad87df
Pascal Garber <pascal@artandcode.studio>
2025-10-22 13:18:24 +0200

    refactor(email): Format EmailEditor

--- apps/frontend/src/components/pages/admin/membership-builder/EmailEditor.vue
+++ apps/frontend/src/components/pages/admin/membership-builder/EmailEditor.vue
@@ -118,14 +118,14 @@ import {
   AppSubHeading,
 } from '@beabee/vue';
 
+import type {
+  EditableEmailTemplate,
+  EmailPreviewResult,
+  EmailServerRenderConfig,
+} from '@type/email-editor';
 import { client } from '@utils/api';
 import { computed, reactive, ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
-import type {
-  EditableEmailTemplate,
-  EmailServerRenderConfig,
-  EmailPreviewResult,
-} from '@type/email-editor';
 
 // Props definition with clearer naming and organization
 const props = withDefaults(
