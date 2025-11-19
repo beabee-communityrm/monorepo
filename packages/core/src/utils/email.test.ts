92f8aab428373fb355e2a908242fb1f8682256fd
Pascal Garber <pascal@artandcode.studio>
2025-11-19 13:25:16 +0100

    refactor: move email validation tests tocalidations.test.ts in common

--- packages/core/src/utils/email.test.ts
+++ packages/core/src/utils/email.test.ts
@@ -1,62 +1,10 @@
-import { isEmail } from '@beabee/beabee-common';
-
-import { describe, expect, it, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import {
   expandNestedMergeFields,
   normalizeEmailAddress,
   replaceMergeFields,
 } from './email';
-
-describe('email validation', () => {
-  test('valid email', () => {
-    expect(isEmail('test@example.com')).toBe(true);
-  });
-
-  test('valid email with subdomain', () => {
-    expect(isEmail('test@sub.example.com')).toBe(true);
-  });
-
-  test('valid email with number', () => {
-    expect(isEmail('test123@example.com')).toBe(true);
-  });
-
-  test('valid email with hyphen', () => {
-    expect(isEmail('test-test@example.com')).toBe(true);
-  });
-
-  test('valid email with underscore', () => {
-    expect(isEmail('test_test@example.com')).toBe(true);
-  });
-
-  test('valid email with dot before @', () => {
-    expect(isEmail('test.test@example.com')).toBe(true);
-  });
-
-  test('invalid email missing @', () => {
-    expect(isEmail('testexample.com')).toBe(false);
-  });
-
-  test('invalid email missing domain', () => {
-    expect(isEmail('test@')).toBe(false);
-  });
-
-  test('invalid email missing username', () => {
-    expect(isEmail('@example.com')).toBe(false);
-  });
-
-  test('invalid email missing top-level domain', () => {
-    expect(isEmail('test@example')).toBe(false);
-  });
-
-  test('invalid email contains spaces', () => {
-    expect(isEmail('test @example.com')).toBe(false);
-  });
-
-  test('invalid email contains multiple @', () => {
-    expect(isEmail('test@test@example.com')).toBe(false);
-  });
-});
 
 describe('normalizeEmailAddress', () => {
   it('should trim whitespace from email', () => {
