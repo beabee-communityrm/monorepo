beabee-communityrm/monorepo

commit dcd6ae3d056ec60353a413c9511d3a10f07f9a8f

fix(cnrMode): callouts should always be OnlyAnonymous on CNR mode

apps/frontend/src/env.ts
@@ -1,3 +1,8 @@
+const cnrModeRaw = '__cnrMode__' as unknown as boolean | string;
+const experimentalFeaturesRaw = '__experimentalFeatures__' as unknown as
+  | boolean
+  | string;
+
 export default {
   /**
    * Application base URL
@@ -83,7 +88,7 @@ export default {
    * Enables CrowdNewsroom specific features and UI elements
    * Set to 'true' or any non-empty value to enable
    */
-  cnrMode: '__cnrMode__',
+  cnrMode: cnrModeRaw === 'true' || cnrModeRaw === true,
 
   /**
    * Comma-separated list of experimental features to enable
@@ -94,5 +99,6 @@ export default {
    * Example: 'feature1,feature2,feature3'
    * Shows features that are still in development
    */
-  experimentalFeatures: '__experimentalFeatures__',
+  experimentalFeatures:
+    experimentalFeaturesRaw === 'true' || experimentalFeaturesRaw === true,
 };

apps/frontend/src/utils/callouts.ts
@@ -438,13 +438,19 @@ export function convertStepsToCallout(
   const slides = convertSlidesForCallout(tabs);
   const variants = convertVariantsForCallout(tabs);
 
-  const access = tabs.settings.openToEveryone
-    ? tabs.settings.collectInfo
-      ? tabs.settings.collectGuestInfo
-        ? CalloutAccess.Guest
-        : CalloutAccess.Anonymous
-      : CalloutAccess.OnlyAnonymous
-    : CalloutAccess.Member;
+  let access: CalloutAccess;
+
+  if (env.cnrMode) {
+    access = CalloutAccess.OnlyAnonymous;
+  } else {
+    access = tabs.settings.openToEveryone
+      ? tabs.settings.collectInfo
+        ? tabs.settings.collectGuestInfo
+          ? CalloutAccess.Guest
+          : CalloutAccess.Anonymous
+        : CalloutAccess.OnlyAnonymous
+      : CalloutAccess.Member;
+  }
 
   return {
     slug: slug || undefined,

commit d870c5a413aea1ae87581a5a191c71bfd80ec30f

fix(frontend-env): fix broken CNR_MODE detection
Reverts change introduced in dcd6ae3d056ec60353a413c9511d3a10f07f9a8f.
Minification breaks the conversion to boolean (it collapses to being
always false)

apps/frontend/src/env.ts
@@ -1,4 +1,3 @@
-const cnrModeRaw = '__cnrMode__' as unknown as boolean | string;
 export default {
   /**
    * Application base URL
@@ -84,5 +83,5 @@ export default {
    * Enables CrowdNewsroom specific features and UI elements
    * Set to 'true' or any non-empty value to enable
    */
-  cnrMode: cnrModeRaw === 'true' || cnrModeRaw === true,
+  cnrMode: '__cnrMode__',
 };