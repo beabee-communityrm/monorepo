e09168032772fc54f00ee81e4a3d365123475532
Camille Koenders <camille.koenders@correctiv.org>
2025-12-04 18:11:14 +0100

    feat(frontend): add one time donation for to home page (not yet configurable)

--- apps/frontend/src/pages/profile/index.vue
+++ apps/frontend/src/pages/profile/index.vue
@@ -42,7 +42,7 @@ meta:
     }}</AppButton>
   </section>
 
-  <section>
+  <section class="mb-6 lg:mr-6">
     <SectionTitle>{{ t('homePage.yourProfile') }}</SectionTitle>
 
     <div class="mb-4 flex">
@@ -56,25 +56,56 @@ meta:
       >{{ t('homePage.manageContribution') }}</AppButton
     >
   </section>
+  <section class="mb-6 lg:mr-6 lg:w-1/4">
+    <form @submit.prevent="handleSubmitDonation">
+      <SectionTitle>{{ t('homePage.makeOneTimeDonation') }}</SectionTitle>
+
+      <AppContribution
+        v-model:amount="oneTimeDonation.amount"
+        v-model:period="oneTimeDonation.period"
+        v-model:pay-fee="oneTimeDonation.payFee"
+        v-model:payment-method="oneTimeDonation.paymentMethod"
+        :content="content"
+        :payment-content="paymentContent"
+        :show-period="false"
+      >
+      </AppContribution>
+
+      <!-- <AppNotification
+      class="mb-4"
+      variant="error"
+      :title="t('contribution.contributionUpdateError')"
+    /> -->
+
+      <AppButton variant="link" class="mb-4 mt-4 w-full">
+        Make a Donation
+      </AppButton>
+    </form>
+  </section>
 </template>
 
 <script lang="ts" setup>
 import {
+  type ContentPaymentData,
   type ContentProfileData,
   type GetCalloutData,
   type GetContactData,
   ItemStatus,
+  PaymentMethod,
+  type PaymentPeriod,
 } from '@beabee/beabee-common';
 import { AppButton, PageTitle, WelcomeMessage } from '@beabee/vue';
 
 import CalloutCard from '@components/callout/CalloutCard.vue';
+import AppContribution from '@components/contribution/AppContribution.vue';
 import ContributionInfo from '@components/pages/profile/ContributionInfo.vue';
 import NoticeContainer from '@components/pages/profile/NoticeContainer.vue';
 import QuickActions from '@components/pages/profile/QuickActions.vue';
 import SectionTitle from '@components/pages/profile/SectionTitle.vue';
 import { currentUser, generalContent } from '@store';
+import type { OneTimePaymentContent } from '@type/one-time-payment-content';
 import { client } from '@utils/api';
-import { type Ref, onBeforeMount, ref } from 'vue';
+import { type Ref, onBeforeMount, reactive, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRoute } from 'vue-router';
 
@@ -92,14 +123,67 @@ const profileContent = ref<ContentProfileData>({
   introMessage: '',
 });
 
+// const stripeClientSecret = ref('');
+
 const callouts = ref<GetCalloutData[]>([]);
 
 // This page is behind auth so currentUser can't be null
 // TODO: is there a nicer way to handle this?
 const user = currentUser as Ref<GetContactData>;
 
+const content = ref<OneTimePaymentContent>({
+  initialAmount: 5,
+  minMonthlyAmount: 5,
+  showAbsorbFee: true,
+  paymentMethods: [PaymentMethod.StripeCard],
+});
+
+const paymentContent = ref<ContentPaymentData>({
+  stripePublicKey: '',
+  stripeCountry: 'eu',
+  taxRate: 0,
+  taxRateEnabled: false,
+  noticeText: '',
+});
+
+const oneTimeDonation = reactive({
+  amount: 5,
+  period: 'one-time' as PaymentPeriod,
+  payFee: true,
+  prorate: true, // TODO: check if this is correct
+  paymentMethod: PaymentMethod.StripeCard,
+});
+
+const loading = ref(false);
+
+async function handleSubmitDonation() {
+  loading.value = true;
+
+  try {
+    // TODO: add correct endpoint as soon as it is done
+    // const clientData: CreateOneTimePaymentData = {
+    //   amount: oneTimeDonation.amount,
+    //   paymentMethod: oneTimeDonation.paymentMethod,
+    //   payFee: oneTimeDonation.payFee,
+    //   completeUrl: client.signup.completeUrl,
+    // };
+    // const data = await client.contact.contribution.start(clientData);
+    // if (data.redirectUrl) {
+    //   window.location.href = data.redirectUrl;
+    // } else if (data.clientSecret) {
+    //   stripeClientSecret.value = data.clientSecret;
+    // }
+  } finally {
+    loading.value = false;
+  }
+}
+
 onBeforeMount(async () => {
   profileContent.value = await client.content.get('profile');
+
+  content.value = await client.content.get('join');
+  paymentContent.value = await client.content.get('payment');
+
   callouts.value = (
     await client.callout.list({
       order: 'DESC',
