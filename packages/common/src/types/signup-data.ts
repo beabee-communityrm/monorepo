deb393c352cbb6ae511572fc203e9fdbf4171dda
Will Franklin <will@willpf.co.uk>
2025-12-05 17:31:06 +0100

    feat(payment): update frontend types to reflect API requirements

--- packages/common/src/types/signup-data.ts
+++ packages/common/src/types/signup-data.ts
@@ -1,21 +1,10 @@
 import type {
-  PaymentMethod,
+  CreatePaymentData,
   StartContributionData,
 } from '@beabee/beabee-common';
 
-export interface SignupContributionData extends StartContributionData {
-  paymentMethod: PaymentMethod;
-}
-
-export interface SignupOneTimePaymentData {
-  amount: number;
-  payFee: boolean;
-  paymentMethod: PaymentMethod;
-  completeUrl: string;
-}
-
 export interface SignupData {
   email: string;
-  contribution?: SignupContributionData;
-  oneTimePayment?: SignupOneTimePaymentData;
+  contribution?: StartContributionData;
+  oneTimePayment?: CreatePaymentData;
 }
--- packages/common/src/types/start-contribution-data.ts
+++ packages/common/src/types/start-contribution-data.ts
@@ -2,6 +2,6 @@ import type { PaymentMethod } from '../data/index.js';
 import type { SetContributionData } from './index.js';
 
 export interface StartContributionData extends SetContributionData {
-  paymentMethod?: PaymentMethod;
+  paymentMethod: PaymentMethod;
   completeUrl: string;
 }
