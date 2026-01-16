0bcc2776db5cf984552e1f909d23c6a7788098dd
Will Franklin <will@willpf.co.uk>
2026-01-16 12:37:24 +0100

    docs(mc-api): add docs for MC API methods

--- packages/core/src/lib/mailchimp.ts
+++ packages/core/src/lib/mailchimp.ts
@@ -20,6 +20,12 @@ import { extractJsonArchive } from '#utils/file';
 
 const log = mainLogger.child({ app: 'mailchimp' });
 
+/**
+ * Create a Mailchimp API instance
+ *
+ * @param settings The Mailchimp API settings
+ * @returns A Mailchimp API instance
+ */
 export function createInstance(
   settings: MailchimpNewsletterConfig['settings']
 ) {
@@ -59,12 +65,25 @@ export function createInstance(
     }
   );
 
+  /**
+   * Create a batch with the given operations
+   *
+   * @param operations The operations to include in the batch
+   * @returns
+   */
   async function createBatch(operations: MCOperation[]): Promise<MCBatch> {
     log.info(`Creating batch with ${operations.length} operations`);
     const response = await instance.post('/batches/', { operations });
     return response.data as MCBatch;
   }
 
+  /**
+   * Wait for the given batch to finish processing. Polls the batch status every
+   * 5 seconds until it is finished, then returns the finished batch.
+   *
+   * @param batch The batch
+   * @returns The finished batch
+   */
   async function waitForBatch(batch: MCBatch): Promise<MCBatch> {
     log.info(`Waiting for batch ${batch.id}`, {
       finishedOperations: batch.finished_operations,
@@ -82,6 +101,16 @@ export function createInstance(
     }
   }
 
+  /**
+   * Processes the batch response. Mailchimp provides a URL to download a tar
+   * archive with the responses for each operation in the batch. Check each
+   * response for errors, optionally specifying which statuses are expected, and
+   * return the parsed response bodies.
+   *
+   * @param batch The finished batch
+   * @param validateStatus Optional function to validate the status code of each operation
+   * @returns Array of operation responses
+   */
   async function getBatchResponses<T = unknown>(
     batch: MCBatch,
     validateStatus?: (status: number) => boolean
@@ -114,6 +143,18 @@ export function createInstance(
     });
   }
 
+  /**
+   * Dispatches a single operation to the Mailchimp API, optionally specifying
+   * which statuses are expected. This can be used to specify that some error
+   * codes are expected although only 2xx codes will return a body.
+   *
+   * For example, when fetching a member that might not exist, a 404 shouldn't
+   * throw an exception.
+   *
+   * @param operation The operation
+   * @param validateStatus Optional function to validate the status code
+   * @returns The parsed response body or null if the status is non 2xx
+   */
   async function dispatchOperation<T = unknown>(
     operation: MCOperation,
     validateStatus?: (status: number) => boolean
@@ -128,6 +169,14 @@ export function createInstance(
     return resp.status >= 200 && resp.status < 300 ? (resp.data as T) : null;
   }
 
+  /**
+   * Dispatches multiple operations to the Mailchimp API, either as individual
+   * requests or as a batch if there are more than 20 operations.
+   *
+   * @param operations The operations
+   * @param validateStatus Optional function to validate the status code
+   * @returns Array of operation responses
+   */
   async function dispatchOperations<T = unknown>(
     operations: MCOperation[],
     validateStatus?: (status: number) => boolean
@@ -168,6 +217,12 @@ export function createInstance(
   };
 }
 
+/**
+ * Convert a Mailchimp status to a NewsletterStatus
+ *
+ * @param mcStatus The Mailchimp status
+ * @returns The NewsletterStatus
+ */
 export function mcStatusToStatus(mcStatus: MCStatus): NewsletterStatus {
   switch (mcStatus) {
     case 'cleaned':
@@ -186,6 +241,13 @@ export function mcStatusToStatus(mcStatus: MCStatus): NewsletterStatus {
   }
 }
 
+/**
+ * Get the Mailchimp member URL for the given list ID and email address
+ *
+ * @param listId The Mailchimp list/audience ID
+ * @param email The email address
+ * @returns The Mailchimp member URL
+ */
 export function getMCMemberUrl(listId: string, email: string) {
   const emailHash = crypto
     .createHash('md5')
@@ -194,6 +256,12 @@ export function getMCMemberUrl(listId: string, email: string) {
   return `lists/${listId}/members/${emailHash}`;
 }
 
+/**
+ * Convert a NewsletterContact to a Mailchimp member object
+ *
+ * @param nlContact The newsletter contact
+ * @returns The Mailchimp member object
+ */
 export function nlContactToMCMember(
   nlContact: UpdateNewsletterContact
 ): Partial<MCMember> {
@@ -225,12 +293,19 @@ export function nlContactToMCMember(
   };
 }
 
+/**
+ * Convert a Mailchimp member object to a NewsletterContact
+ *
+ * @param member The Mailchimp member
+ * @returns The newsletter contact
+ */
 export function mcMemberToNlContact(member: MCMember): NewsletterContact {
   const { FNAME, LNAME, ...fields } = member.merge_fields;
   const activeMemberTag = OptionsService.getText(
     'newsletter-active-member-tag'
   );
   const activeUserTag = OptionsService.getText('newsletter-active-user-tag');
+
   return {
     email: normalizeEmailAddress(member.email_address),
     firstname: FNAME || '',
