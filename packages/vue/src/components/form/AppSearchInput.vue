c3c676a4f506f99dc7293cc1e14ba8ddc4921e7c
Pascal Garber <pascal@artandcode.studio>
2026-01-30 16:44:01 +0100

    feat(locale): add 'clearSearch' action to multiple locales and update AppSearchInput component

--- packages/vue/src/components/form/AppSearchInput.vue
+++ packages/vue/src/components/form/AppSearchInput.vue
@@ -1,12 +1,13 @@
 <template>
-  <form @submit.prevent="submit">
+  <form class="w-full max-w-md" @submit.prevent="submit">
     <AppInput v-model="searchText" :placeholder="placeholder" @blur="submit">
       <template #suffixAction>
-        <div class="flex h-10 items-center">
+        <div class="flex h-10 shrink-0 items-center">
           <button
             v-if="searchText.length > 0"
             type="button"
             class="flex h-10 w-10 shrink-0 items-center justify-center text-primary-80 hover:bg-primary-10 focus:outline-none"
+            :aria-label="t('actions.clearSearch')"
             @click="emit('update:modelValue', '')"
           >
             <font-awesome-icon :icon="faTimes" />
@@ -14,6 +15,8 @@
           <button
             type="submit"
             class="flex h-10 w-10 shrink-0 items-center justify-center text-primary-80 hover:bg-primary-10 focus:outline-none"
+            :aria-label="t('actions.search')"
+            :class="searchText.length > 0 && 'border-l border-primary-40'"
           >
             <font-awesome-icon :icon="faSearch" />
           </button>
@@ -26,8 +29,11 @@
 <script lang="ts" setup>
 import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { ref, watch } from 'vue';
+import { useI18n } from 'vue-i18n';
 
 import AppInput from './AppInput.vue';
+
+const { t } = useI18n();
 
 /**
  * Props for the AppSearchInput component
