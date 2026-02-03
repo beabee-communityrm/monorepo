c1505a070d47499e3665619c3318170a70b98826
Pascal Garber <pascal@artandcode.studio>
2026-02-03 11:16:18 +0100

    style(vue/notification): update button styles and layout in AppNotificationContainer story

--- packages/vue/src/components/notification/AppNotificationContainer.story.vue
+++ packages/vue/src/components/notification/AppNotificationContainer.story.vue
@@ -31,27 +31,27 @@ const addTestNotification = (
         <AppNotificationContainer />
 
         <!-- Control area -->
-        <div class="absolute left-4 top-4 space-y-2">
+        <div class="absolute bottom-4 left-4 flex flex-wrap gap-2">
           <button
-            class="bg-green-500 rounded px-3 py-1 text-white"
+            class="rounded bg-success-70 px-3 py-1 font-semibold text-white shadow-sm"
             @click="addTestNotification('success')"
           >
             Add Success
           </button>
           <button
-            class="bg-yellow-500 rounded px-3 py-1 text-white"
+            class="rounded bg-warning-70 px-3 py-1 font-semibold text-white shadow-sm"
             @click="addTestNotification('warning')"
           >
             Add Warning
           </button>
           <button
-            class="bg-red-500 rounded px-3 py-1 text-white"
+            class="rounded bg-danger-70 px-3 py-1 font-semibold text-white shadow-sm"
             @click="addTestNotification('error')"
           >
             Add Error
           </button>
           <button
-            class="bg-blue-500 rounded px-3 py-1 text-white"
+            class="rounded bg-primary-70 px-3 py-1 font-semibold text-white shadow-sm"
             @click="addTestNotification('info')"
           >
             Add Info
