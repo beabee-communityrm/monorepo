0ea01ebbc0fe44338ed890bf70c636900f907ab9
Will Franklin <will@willpf.co.uk>
2026-02-06 12:03:54 +0100

    fix(ms-builder): allow disabling welcome message

--- apps/frontend/src/pages/admin/membership-builder/intro.vue
+++ apps/frontend/src/pages/admin/membership-builder/intro.vue
@@ -6,40 +6,39 @@ meta:
 </route>
 
 <template>
-  <App2ColGrid>
-    <template #col1>
-      <p class="mb-8">{{ stepT('text') }}</p>
-      <AppCheckbox
-        v-model="showIntroMessage"
-        :label="stepT('showWelcomeMessage')"
-        class="mb-4 font-semibold"
-      />
-    </template>
-  </App2ColGrid>
-  <App2ColGrid v-if="showIntroMessage" extended>
-    <template #col1>
-      <AppApiForm
-        :button-text="t('form.saveChanges')"
-        :success-text="t('form.saved')"
-        @submit="handleUpdate"
-      >
+  <AppApiForm
+    :button-text="t('form.saveChanges')"
+    :success-text="t('form.saved')"
+    @submit="handleUpdate"
+  >
+    <App2ColGrid>
+      <template #col1>
+        <p class="mb-8">{{ stepT('text') }}</p>
+        <AppCheckbox
+          v-model="showIntroMessage"
+          :label="stepT('showWelcomeMessage')"
+          class="mb-4 font-semibold"
+        />
+      </template>
+    </App2ColGrid>
+    <App2ColGrid v-if="showIntroMessage" class="mb-6" extended>
+      <template #col1>
         <AppRichTextEditor
           v-model="introMessage"
           :label="stepT('message')"
-          class="mb-4"
           required
         />
-      </AppApiForm>
-    </template>
-    <template #col2>
-      <WelcomeMessage
-        :first-name="currentUser?.firstname || ''"
-        :last-name="currentUser?.lastname || ''"
-        :text="introMessage"
-        small
-      />
-    </template>
-  </App2ColGrid>
+      </template>
+      <template #col2>
+        <WelcomeMessage
+          :first-name="currentUser?.firstname || ''"
+          :last-name="currentUser?.lastname || ''"
+          :text="introMessage"
+          small
+        />
+      </template>
+    </App2ColGrid>
+  </AppApiForm>
 </template>
 <script lang="ts" setup>
 import {
