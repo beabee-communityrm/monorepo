db04ec91033e6099e5a8f16e4ecd4b07e7854913
Will Franklin <will@willpf.co.uk>
2026-03-06 14:35:33 +0100

    fix(stripe-payment): incorrectly starting downgrades immediately

--- packages/core/src/lib/stripe.ts
+++ packages/core/src/lib/stripe.ts
@@ -132,7 +132,8 @@ async function calculateProrationParams(
     // Only prorate amounts above 100 cents. This aligns with GoCardless's minimum
     // amount and is much simpler than trying to calculate the minimum payment per
     // payment method
-    prorationAmount: prorationAmount < 100 ? 0 : prorationAmount,
+    prorationAmount:
+      prorationAmount >= 0 && prorationAmount < 100 ? 0 : prorationAmount,
     prorationTime,
   };
 }
@@ -224,7 +225,8 @@ export async function updateSubscription(
     await stripe.subscriptionSchedules.release(oldSchedule.id);
   }
 
-  const startNow = prorationAmount === 0 || form.prorate;
+  const startNow =
+    prorationAmount === 0 || (prorationAmount > 0 && form.prorate);
 
   if (startNow) {
     const params: Stripe.SubscriptionUpdateParams = {
