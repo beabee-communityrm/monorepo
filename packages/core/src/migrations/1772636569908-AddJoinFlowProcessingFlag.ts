c91afba5411e77c72e9061382dae43c3f763928a
Will Franklin <will@willpf.co.uk>
2026-03-13 16:03:54 +0100

    chore(migration): make migration fail silently if table is no longer there

--- packages/core/src/migrations/1772636569908-AddJoinFlowProcessingFlag.ts
+++ packages/core/src/migrations/1772636569908-AddJoinFlowProcessingFlag.ts
@@ -7,11 +7,13 @@ export class AddJoinFlowProcessingFlag1772636569908
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "join_flow" ADD "processing" boolean NOT NULL DEFAULT false`
+      `ALTER TABLE IF EXISTS "join_flow" ADD "processing" boolean NOT NULL DEFAULT false`
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "join_flow" DROP COLUMN "processing"`);
+    await queryRunner.query(
+      `ALTER TABLE IF EXISTS "join_flow" DROP COLUMN "processing"`
+    );
   }
 }
