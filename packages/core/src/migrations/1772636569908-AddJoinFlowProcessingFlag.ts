fd0c39060cc29ba92bb5cab94056d906cb47cec3
Will Franklin <will@willpf.co.uk>
2026-03-13 17:42:48 +0100

    fix(migration): add to both tables as they could both be there

--- packages/core/src/migrations/1772636569908-AddJoinFlowProcessingFlag.ts
+++ packages/core/src/migrations/1772636569908-AddJoinFlowProcessingFlag.ts
@@ -9,11 +9,17 @@ export class AddJoinFlowProcessingFlag1772636569908
     await queryRunner.query(
       `ALTER TABLE IF EXISTS "join_flow" ADD "processing" boolean NOT NULL DEFAULT false`
     );
+    await queryRunner.query(
+      `ALTER TABLE IF EXISTS "payment_flow" ADD "processing" boolean NOT NULL DEFAULT false`
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE IF EXISTS "join_flow" DROP COLUMN "processing"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE IF EXISTS "payment_flow" DROP COLUMN "processing"`
     );
   }
 }
