5bf043974eaa00422115974c8e155b0973d32475
Will Franklin <will@willpf.co.uk>
2026-04-23 17:21:33 +0200

    fix(migration): should handle no newsletter-groups

--- packages/core/src/migrations/1776955640900-AddMailchimpNLGroups.ts
+++ packages/core/src/migrations/1776955640900-AddMailchimpNLGroups.ts
@@ -6,8 +6,12 @@ export class AddMailchimpNLGroups1776955640900 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO option ("key", "value") VALUES ('mailchimp-newsletter-groups',
-        (SELECT string_agg(elem->>'id', ',')
-         FROM jsonb_array_elements((SELECT "value" FROM "option" WHERE "key" = 'newsletter-groups')::jsonb) AS elem)
+        COALESCE(
+          (SELECT string_agg(elem->>'id', ',')
+           FROM jsonb_array_elements((SELECT "value" FROM "option" WHERE "key" = 'newsletter-groups')::jsonb) AS elem
+           WHERE elem->>'id' IS NOT NULL),
+          ''
+        )
       ) ON CONFLICT DO NOTHING`
     );
   }
