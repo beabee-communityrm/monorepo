b09be658e8b09cca07cfd6f21a25fab2f1ef13b3
Aastha <aastha.sharma@correctiv.org>
2026-05-06 15:52:14 +0200

    feat (payment-icons): Add icons for Google Pay and Apple Pay

---
+++ apps/frontend/src/components/payment/icons/GooglePay.vue
@@ -0,0 +1,12 @@
+<!--
+  # Google Pay Icon
+  Standard Google pay payment method icon.
+  Uses Font Awesome credit card icon.
+-->
+<template>
+  <font-awesome-icon :icon="faGooglePay" />
+</template>
+
+<script lang="ts" setup>
+import { faGooglePay } from '@fortawesome/free-brands-svg-icons';
+</script>
