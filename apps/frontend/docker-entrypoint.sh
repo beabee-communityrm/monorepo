c8ec472d7ca88515d015c130f63c0e5ac524bf9d
Aastha <aastha.sharma@correctiv.org>
2026-05-06 16:30:53 +0200

    Print env variables, enabled debugger mode, put sed command in a single line

--- apps/frontend/docker-entrypoint.sh
+++ apps/frontend/docker-entrypoint.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
+set -x # enable debugging mode
+export # display all environment variables known to the shell currently
+
 if [ ! -f /.installed ]; then
   touch /.installed
 
   # Replace environment variables in the frontend build
   # Keep this in sync with vite.config.ts
   # Please note that these placeholders only exist if Vite is built for production, otherwise Vite already replaces them
-  find /usr/share/nginx/html -type f -name '*.js' -exec \
-    sed -i \
-      -e s#__appUrl__#$APP_BASE_URL#g \
-      -e s#__apiUrl__#$API_BASE_URL#g \
-      -e s#__revision__#$APP_REVISION#g \
-      -e s#__version__#$APP_VERSION#g \
-      -e s#__appsignalKey__#$APPSIGNAL_KEY#g \
-      -e s#__captchafoxKey__#$CAPTCHAFOX_KEY#g \
-      -e s#__maptilerKey__#$MAPTILER_KEY#g \
-      -e s#__cnrMode__#$CNR_MODE#g \
-    {} +
+  find /usr/share/nginx/html -type f -name '*.js' -exec sed -i -e "s|__appUrl__|$APP_BASE_URL|g" -e "s|__apiUrl__|$API_BASE_URL|g" -e "s|__revision__|$APP_REVISION|g" -e "s|__version__|$APP_VERSION|g" -e "s|__appsignalKey__|$APPSIGNAL_KEY|g" -e "s|__captchafoxKey__|$CAPTCHAFOX_KEY|g" -e "s|__maptilerKey__|$MAPTILER_KEY|g" -e "s|__cnrMode__|$CNR_MODE|g" {} +
 fi
