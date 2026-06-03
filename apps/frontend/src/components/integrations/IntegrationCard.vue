2069d6be9b928e7fcc7edb9f8a7b438b950911a5
libby-correctiv <olivia.rear@correctiv.org>
2026-06-03 15:05:06 +0200

    ui for integration tab with mock data

---
+++ apps/frontend/src/components/integrations/IntegrationCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="rounded border border-primary-20 bg-white">
+    <IntegrationCardHeader :integration="integration" />
+    <div v-if="hasBody" class="border-t border-primary-20 p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, useSlots } from 'vue';
+
+import type { Integration } from '#type/integration';
+import IntegrationCardHeader from './IntegrationCardHeader.vue';
+
+defineProps<{ integration: Integration }>();
+
+const slots = useSlots();
+
+const hasBody = computed(() => !!slots.default?.().length);
+</script>
