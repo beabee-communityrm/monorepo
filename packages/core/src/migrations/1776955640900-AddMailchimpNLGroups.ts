9cecee2235fb80baa5d1497c5f34c4aaed91477d
Aastha <aastha.sharma@correctiv.org>
2026-06-09 12:12:03 +0200

    (migrations) Revert changes to old migration

--- packages/core/src/migrations/1776955640900-AddMailchimpNLGroups.ts
+++ packages/core/src/migrations/1776955640900-AddMailchimpNLGroups.ts
@@ -7,13 +7,10 @@ export class AddMailchimpNLGroups1776955640900 implements MigrationInterface {
     await queryRunner.query(
       `INSERT INTO option ("key", "value") VALUES ('mailchimp-newsletter-groups',
         COALESCE(
-          (SELECT jsonb_agg(jsonb_build_object(
-             'id', elem->>'id',
-             'label', elem->>'label'
-           ))::text
+          (SELECT string_agg(elem->>'id', ',')
            FROM jsonb_array_elements((SELECT "value" FROM "option" WHERE "key" = 'newsletter-groups')::jsonb) AS elem
            WHERE elem->>'id' IS NOT NULL),
-          '[]'
+          ''
         )
       ) ON CONFLICT DO NOTHING`
     );
