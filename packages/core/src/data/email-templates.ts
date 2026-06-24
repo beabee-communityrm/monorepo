3d809e0ad1a7180587ad0500cc78e8c36dadd627
Aastha <aastha.sharma@correctiv.org>
2026-06-24 13:32:21 +0200

    Add new email template
    Automated notification email for when cached groups are deleted due to a
    change in newsletter provider
    
    Add German translations for email

--- packages/core/src/data/email-templates.ts
+++ packages/core/src/data/email-templates.ts
@@ -221,6 +221,14 @@ export const adminEmailTemplates = {
       MEMBERNAME: params.contact.fullname,
     })
   ),
+  'deleted-group': withMergeFields(
+    ['GROUPS'],
+    (params: { groups: { id: string; label: string }[] }) => ({
+      GROUPS: params.groups
+        .map((g) => `<li>${g.label} (${g.id})</li>`)
+        .join(''),
+    })
+  ),
   'new-callout-response': withMergeFields(
     ['CALLOUTSLUG', 'CALLOUTTITLE', 'RESPNAME'],
     (params: {
