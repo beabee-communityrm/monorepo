ba7447007a39adf0373b7faf9091d8f21b0ee262
Aastha <aastha.sharma@correctiv.org>
2026-07-13 15:35:01 +0200

    Get translated labels for callout sidebar

--- apps/frontend/src/pages/admin/crowdnewsroom/view/[id]/responses/index.vue
+++ apps/frontend/src/pages/admin/crowdnewsroom/view/[id]/responses/index.vue
@@ -251,6 +251,7 @@ import { addBreadcrumb } from '#store/breadcrumb';
 import { canAdmin } from '#store/currentUser';
 import { client } from '#utils/api';
 import { extractErrorText } from '#utils/api-error';
+import { buckets } from '#utils/callouts';
 import {
   definePaginatedQuery,
   defineParam,
@@ -420,11 +421,18 @@ async function updateSegment(
 }
 
 async function listSegments() {
-  return await client.callout.segments.list(
+  const segments = await client.callout.segments.list(
     props.callout.slug,
     { sort: 'order' },
     ['itemCount']
   );
+  return segments.map((segment) => ({
+    ...segment,
+    name:
+      buckets.value.find(
+        (bucket) => bucket.id.toLowerCase() === segment.name.toLowerCase()
+      )?.label ?? segment.name,
+  }));
 }
 
 async function listTotalSegmentItems() {
