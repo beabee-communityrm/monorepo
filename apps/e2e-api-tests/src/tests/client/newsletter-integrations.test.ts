c445a94ab87c632746707b308d217feb84e7893a
Aastha <aastha.sharma@correctiv.org>
2026-07-14 10:32:07 +0200

    (e2e-api-test) Check if contact subscription and join content are cleaned up

--- apps/e2e-api-tests/src/tests/client/newsletter-integrations.test.ts
+++ apps/e2e-api-tests/src/tests/client/newsletter-integrations.test.ts
@@ -1,14 +1,18 @@
-import { ApiHealthStatus } from '@beabee/beabee-common';
-import { IntegrationsClient } from '@beabee/client';
-import { api, testUser } from '@beabee/test-utils/test-data';
+import { ApiHealthStatus, GetContactWith } from '@beabee/beabee-common';
+import { BeabeeClient } from '@beabee/client';
+import {
+  api,
+  rateLimitedTestUser,
+  testUser,
+} from '@beabee/test-utils/test-data';
 
 import { beforeAll, describe, expect, it } from 'vitest';
 
 describe('Newsletter integrations API', () => {
-  let client: IntegrationsClient;
+  let client: BeabeeClient;
 
   beforeAll(() => {
-    client = new IntegrationsClient({
+    client = new BeabeeClient({
       host: api.host,
       path: api.path,
       token: testUser.apiKey,
@@ -18,7 +22,9 @@ describe('Newsletter integrations API', () => {
   describe('Provider Info', () => {
     it('should return newsletter provider info', async () => {
       try {
-        const providerInfo = await client.getNewsletter(['health']);
+        const providerInfo = await client.integrations.getNewsletter([
+          'health',
+        ]);
         expect(providerInfo).toBeDefined();
         expect(providerInfo.provider).toBe('test');
         expect(providerInfo.status).toBe(ApiHealthStatus.HEALTHY);
@@ -28,7 +34,8 @@ describe('Newsletter integrations API', () => {
     });
     it('should get cached newsletter groups', async () => {
       try {
-        const newsletterGroups = await client.getNewsletterGroups();
+        const newsletterGroups =
+          await client.integrations.getNewsletterGroups();
         expect(newsletterGroups).toBeDefined();
         expect(newsletterGroups).not.toHaveLength(0);
       } catch (error) {
@@ -37,18 +44,34 @@ describe('Newsletter integrations API', () => {
     });
     it('should refresh and remove groups', async () => {
       try {
-        const { info, groupChanges } = await client.refreshNewsletterGroups();
-        expect(groupChanges.filter((g) => g.action === 'added')).toHaveLength(
-          1
-        );
-        expect(groupChanges.filter((g) => g.action === 'removed')).toHaveLength(
-          1
+        const removedGroupId = '7bd89a737b';
+        // 1. Diff should compute that 1 group was added and 1 removed
+        const { info, groupChanges } =
+          await client.integrations.refreshNewsletterGroups();
+
+        expect(groupChanges).toContainEqual(
+          expect.objectContaining({ id: removedGroupId, action: 'removed' })
         );
 
-        // Verify that cache was updated
-        const updatedGroups = await client.getNewsletterGroups();
+        // 2. Database cache should be updated
+        const updatedGroups = await client.integrations.getNewsletterGroups();
         expect(updatedGroups.map((g) => g.id).sort()).toEqual(
           ['b8e4acb751', 'c0b1a133d1', 'd0g6ced973'].sort()
+        );
+
+        // 3. Deleted group should be removed from contact profile
+        const contactProfile = await client.contact.get(
+          rateLimitedTestUser.contactId,
+          [GetContactWith.Profile]
+        );
+        expect(contactProfile.profile.newsletterGroups).not.toContain(
+          removedGroupId
+        );
+
+        // 4. Deleted group should be removed from join/setup
+        const joinSetup = await client.content.get('join/setup');
+        expect(joinSetup.newsletterGroups.map((g) => g.id)).not.toContain(
+          removedGroupId
         );
       } catch (error) {
         throw error;
