961daa8d189bd9e09acb10d38df84cc8d243e140
libby-correctiv <olivia.rear@correctiv.org>
2026-07-15 11:52:14 +0200

    Skeleton styling changes

--- packages/vue/src/components/nuxt-ui/AppFormSkeleton.vue
+++ packages/vue/src/components/nuxt-ui/AppFormSkeleton.vue
@@ -10,13 +10,20 @@
   - `rows` (number, default 3): how many skeleton bars to show.
 -->
 <template>
-  <USkeleton v-for="i in rows" :key="i" class="my-6 h-10 w-full" />
+  <div class="space-y-8">
+    <template v-for="i in rows" :key="i">
+      <div v-if="i === 2 && rows >= 2" class="flex gap-4">
+        <USkeleton class="h-10 w-1/2" />
+        <USkeleton class="h-10 w-1/2" />
+      </div>
+      <USkeleton v-else class="h-10 w-full" />
+    </template>
+  </div>
 </template>
 
 <script lang="ts" setup>
 /**
- * A stack of skeleton "field" bars for a loading form. See the template
- * comment above.
+ * A stack of skeleton "field" bars for a loading form.
  *
  * @component AppFormSkeleton
  */
