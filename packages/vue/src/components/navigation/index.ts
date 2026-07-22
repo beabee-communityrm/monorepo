e79162d678cbf10abe7847d9ddcd7f1454f93567
libby-correctiv <olivia.rear@correctiv.org>
2026-07-22 14:55:23 +0200

    restore deleted components

--- packages/vue/src/components/navigation/index.ts
+++ packages/vue/src/components/navigation/index.ts
@@ -5,3 +5,8 @@ export { default as AppSlide, type AppSlideProps } from './AppSlide.vue';
 export { default as AppSlider } from './AppSlider.vue';
 
 export { default as AppStepper, type AppStepperProps } from './AppStepper.vue';
+
+export {
+  default as TheBreadcrumb,
+  type BreadcrumbItem,
+} from './TheBreadcrumb.vue';
