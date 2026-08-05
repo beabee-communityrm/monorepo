bd64c16c87fad1841ea46f89412b59119a37486f
Aastha <aastha.sharma@correctiv.org>
2026-08-05 17:11:48 +0200

    feat(contact-data-enrichment) Get sign up source, referrer and campaign information from events table
    If no event is found, or if the information does not exist, empty strings are returned and displayed

--- packages/core/src/services/ActivityService.ts
+++ packages/core/src/services/ActivityService.ts
@@ -1,3 +1,5 @@
+import { ActivityEventType, ContactOriginData } from '@beabee/beabee-common';
+
 import { getRepository } from '#database';
 import { log as mainLogger } from '#logging';
 import { ActivityEvent } from '#models/index';
@@ -32,6 +34,27 @@ class ActivityService {
    */
   async getAllEvents(): Promise<ActivityEvent[]> {
     return getRepository(ActivityEvent).find();
+  }
+
+  /**
+   * Get the origin (source, referrer, campaign) of a contact from their
+   * contact.created activity event
+   * Source is the actor type and referrer and campaign are obtained from
+   * the metadata
+   * @param contactId The contact ID
+   * @returns The contact's origin, or null if no creation event was recorded
+   */
+  async getContactOrigin(targetId: string): Promise<ContactOriginData | null> {
+    const event = await getRepository(ActivityEvent).findOne({
+      where: { targetId, eventType: ActivityEventType.ContactCreated },
+    });
+
+    // If event not found, return empty strings
+    return {
+      source: event ? event.actorType : '',
+      referrer: event ? (event.metadata?.referrer ?? '') : '',
+      campaign: event ? (event.metadata?.campaign ?? '') : '',
+    };
   }
 }
 
