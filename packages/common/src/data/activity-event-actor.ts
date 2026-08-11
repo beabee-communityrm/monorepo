c40fc4c56fb090312d349fb4a3846aef012ac93e
Aastha <aastha.sharma@correctiv.org>
2026-08-11 10:36:30 +0200

    feat(activity-feed) Remove callout actor type, add new types

--- packages/common/src/data/activity-event-actor.ts
+++ packages/common/src/data/activity-event-actor.ts
@@ -1,9 +1,11 @@
 /** Possible types of actors that trigger events in the activity log */
 export enum ActivityActorType {
-  User = 'user', // event triggered by a user (admin or non-admin)
-  System = 'system', // event triggered by system (automated emails, setup steps)
+  User = 'user', // event triggered by a user (admin or non-admin) via cookie
+  ApiKey = 'api-key', // event triggered by a user via api key
+  System = 'system', // event triggered by system
   Webhook = 'webhook', // event triggered by an external integration via webhook
-  Callout = 'callout', // event triggered by a callout (signups, newsletter subscriptions)
+  Cron = 'cron', // event triggered by a cron job
+  BackendCLI = 'backend-cli', // event triggered by the backend CLI
 }
 
 export interface ActivityActor {
