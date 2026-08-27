a434f7677919366f5a07e302e84653ff88d3c8a2
Aastha <aastha.sharma@correctiv.org>
2026-08-27 16:37:54 +0200

    feat(contact-data-enrichment) Add 'addedBy' field to contactorigin interface

--- apps/frontend-old/src/pages/admin/contacts/[id]/index.vue
+++ apps/frontend-old/src/pages/admin/contacts/[id]/index.vue
@@ -201,6 +201,10 @@ meta:
             :name="t('contactOverview.origin.referrer')"
             :value="contact.origin.medium"
           />
+          <AppInfoListItem
+            :name="t('contactOverview.origin.addedBy')"
+            :value="contact.origin.addedBy"
+          />
         </AppInfoList>
       </section>
 
